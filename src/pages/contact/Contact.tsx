848d1df8d29e5f6a5954b1f4a3d7a66dc7073ba7
jsanic16 <julio_san80@aol.com>
2023-10-23 10:40:33 -0600

    adding gallery section

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,10 +1,35 @@
-import './contact.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../assets/images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+        reprehenderit, perferendis cumque doloremque rem officia!
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a href="mailto:julio.sanic.gt.256@gmail.com" target="_blank" rel="noreferrer noopener">
+            <MdEmail />
+            </a>
+            <a href="https://m.me/mafer.miranda.18" target="_blank" rel="noreferrer noopener">
+              <BsMessenger />
+            </a>
+            <a href="https://wa.me/+50258385370" target="_blank" rel="noreferrer noopener">
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,9 +1,37 @@
-import './gallery.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../assets/images/header_bg_3.jpg";
+import image1 from '../../assets/images/gallery1.jpg'
+
+import "./gallery.css";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const galleryLength = 15;
+  const images = [];
 
-export default Gallery
+  for (let i = 0; i < galleryLength; i++) {
+    images.push(`/src/assets/images/gallery${i + 1}.jpg`);
+  }
+
+  console.log(image1)
+  return (
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error aliquam,
+        molestias distinctio cumque culpa sunt?
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {
+            images.map((image, index) => (
+              <article key={image}>
+                  <img src={image} alt={`image ${index}`} />
+              </article>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
