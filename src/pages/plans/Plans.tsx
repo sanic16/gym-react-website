aea1d6edf9ad530bfd5d7911eb235831eec68cc0
jsanic16 <julio_san80@aol.com>
2023-10-23 12:01:09 -0600

    Plans Page added

--- src/pages/plans/Plans.tsx
+++ src/pages/plans/Plans.tsx
@@ -1,10 +1,42 @@
-import './plans.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../assets/images/header_bg_4.jpg";
+import Card from "../../components/ui/Card";
+import { plans } from "../../assets/data";
 
+import "./plans.css";
 
 const Plans = () => {
   return (
-    <div>Plans</div>
-  )
-}
+    <>
+      <Header title="Membership Plans" image={HeaderImage}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt porro
+        quae voluptatem voluptate quaerat blanditiis harum sapiente consequatur
+        temporibus natus?
+      </Header>
+      <section className="plans">
+        <div className="container plans__container">
+          {
+            plans.map(plan => (
+              <Card key={plan.id} className="plan">
+                <h3>{plan.name}</h3>
+                <small>{plan.desc}</small>
+                <h1>{`$${plan.price}`}</h1><h2>/mo</h2>
+                <h4>Features</h4>
+                {
+                  plan.features.map((feature, index) => (
+                    <p key={index} className={!feature.feature ? 'disabled' : ''}>
+                      {feature.feature}
+                    </p>
+                  ))
+                }
+                <button className="btn lg">Choose Plan</button>
+              </Card>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Plans
+export default Plans;
